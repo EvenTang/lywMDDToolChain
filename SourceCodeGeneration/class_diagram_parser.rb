5635ae29760540bf7711c6980727add7472c8560
EvenTang <liyiwen007@gmail.com>
2017-11-29 22:03:51 +0800

    Generate Class diagram

--- SourceCodeGeneration/class_diagram_parser.rb
+++ SourceCodeGeneration/class_diagram_parser.rb
@@ -2,12 +2,57 @@
 
 # All operations should be summarize in class diagrams
 
+
+
+class SendMessageMethod
+  attr_reader :method_name
+  attr_reader :params       # [[type, name], [type, name]]
+  attr_reader :return_type
+
+  # @brief generate a send message method from a planUML seqence statement
+  # 
+  # @param statement string : SetTempareture(NewTemp = 2)
+  def initialize(statement)
+    @params = []
+    @param_values = Hash.new([]) # Hash [name => [value1, value2]]
+    analyzer_send_message_statement(statement)
+  end
+
+  private 
+  def analyzer_send_message_statement(statement)
+    if statement =~ /(\w+)(\(.*\))?/ then
+      @method_name = $1
+      left_str = $2
+      while left_str =~ /((\w+)=([\w"]+)),?(.*)/
+        @params << [:UnknowType, $2]
+        @param_values[$2].push $3
+        left_str = $4
+      end
+    end
+  end
+
+end
+
 class ModuleClass
-  attr_reader :name             # class name
+  attr_reader :module_name             # class name
   attr_reader :private_methods
   attr_reader :public_methods
+  attr_reader :send_message_methods
   attr_reader :data_members
 
+  def initialize(module_name)
+    @module_name = module_name
+    @send_message_methods = []
+  end
+
+  def generate_send_msg_method_from_seq(statement)
+    @send_message_methods << SendMessageMethod.new(statement)
+  end
+
+  def clean_up
+    # FIXME: remove depulicate things and guess types for methods
+    puts "Do nothing right now"
+  end
 
 end
 
