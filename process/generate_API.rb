51ee848957a620632e802a5d4341f22872c4913e
goaigun <aiqunw@kotei-info.com>
2017-12-14 18:18:42 +0800

    Adjust space format

--- process/generate_API.rb
+++ process/generate_API.rb
@@ -8,148 +8,148 @@ require File.expand_path('../MetaModel/component_parser', __FILE__)
 
 
 class ProtoFile
-    attr_accessor :name, :messages, :parent
-    attr_accessor :module_name
+  attr_accessor :name, :messages, :parent
+  attr_accessor :module_name
 
-    def initialize(module_name)
-        @messages = []
-        @parent = self
-        @name = "ProtoFile"
-        @module_name = module_name
-    end
+  def initialize(module_name)
+    @messages = []
+    @parent = self
+    @name = "ProtoFile"
+    @module_name = module_name
+  end
 
-    def Push(element)
-        @messages.push element
-        element.parent = self
-    end
+  def Push(element)
+    @messages.push element
+    element.parent = self
+  end
 
-    def ParseContents(line)
-    end
+  def ParseContents(line)
+  end
 end
 
 class MsgField
-    # field_attribute : const String, one of :required, optional, repeated
-    attr_accessor :name, :type, :field_attribute
+  # field_attribute : const String, one of :required, optional, repeated
+  attr_accessor :name, :type, :field_attribute
 
-    def initialize()
-    end
+  def initialize()
+  end
 
-    def initialize(name, type, field_attr)
-        @name, @type, @field_attribute = name, type, field_attr
-    end
+  def initialize(name, type, field_attr)
+    @name, @type, @field_attribute = name, type, field_attr
+  end
 end
 
 class EnumValue
-    # field_attribute : const String, one of :required, optional, repeated
-    attr_accessor :name, :value
+  # field_attribute : const String, one of :required, optional, repeated
+  attr_accessor :name, :value
 
-    def initialize()
-    end
+  def initialize()
+  end
 
-    def initialize(name, value)
-        @name = name
-        @value = value
-    end
+  def initialize(name, value)
+    @name = name
+    @value = value
+  end
 end
 
 class EnumDef
-    # name : String
-    # values : Hash {ConstName(String) => value(int)}
-    attr_accessor :name, :values
-    attr_accessor :parent  # For parser
+  # name : String
+  # values : Hash {ConstName(String) => value(int)}
+  attr_accessor :name, :values
+  attr_accessor :parent  # For parser
     
-    def initialize(name)
-        @name = name
-        @values = []
-    end
+  def initialize(name)
+    @name = name
+    @values = []
+  end
 
-    def ParseContents(line)
-        if line =~ /(\w+)\s*=\s*(\d+);/
-            @values.push EnumValue.new($1, $2.to_i)
-        end
+  def ParseContents(line)
+    if line =~ /(\w+)\s*=\s*(\d+);/
+      @values.push EnumValue.new($1, $2.to_i)
     end
+  end
 
-    def Push(element)
-        @values.push element
-    end
+  def Push(element)
+    @values.push element
+  end
 end
 
 class OneofField
-    attr_accessor :name, :fields
-    attr_accessor :parent  # For parser
+  attr_accessor :name, :fields
+  attr_accessor :parent  # For parser
     
-    def initialize(name)
-        @name = name
-        @fields = []
-    end
+  def initialize(name)
+    @name = name
+    @fields = []
+  end
 
-    def ParseContents(line)
-        if line =~ /(\w+)\s+(\w+)\s+=\s+(\d+);/
-            @fields.push MsgField.new($2, $1, "")
-        end
+  def ParseContents(line)
+    if line =~ /(\w+)\s+(\w+)\s+=\s+(\d+);/
+      @fields.push MsgField.new($2, $1, "")
     end
+  end
 
-    def Push(element)
-        @fields.push element
-        case element
-        when Message, EnumDef, OneofField 
-            element.parent = self
-        end
+  def Push(element)
+    @fields.push element
+    case element
+    when Message, EnumDef, OneofField 
+      element.parent = self
     end
+  end
 end
 
 class Message
-    attr_accessor :name, :fields
-    attr_accessor :parent  # For parser
+  attr_accessor :name, :fields
+  attr_accessor :parent  # For parser
     
-    def initialize(name)
-        @name = name
-        @fields = []
-    end
+  def initialize(name)
+    @name = name
+    @fields = []
+  end
 
-    def ParseContents(line)
-        if line =~ /(require|optional|repeated)?\s*(\w+)\s+(\w+)\s+=\s+(\d+);/
-            @fields.push MsgField.new($3, $2, $1)
-        end
+  def ParseContents(line)
+    if line =~ /(require|optional|repeated)?\s*(\w+)\s+(\w+)\s+=\s+(\d+);/
+      @fields.push MsgField.new($3, $2, $1)
     end
+  end
 
-    def Push(element)
-        @fields.push element
-        case element
-        when Message, EnumDef, OneofField
-            element.parent = self
-        end
+  def Push(element)
+    @fields.push element
+    case element
+    when Message, EnumDef, OneofField
+      element.parent = self
     end
+  end
 end
 
 class ProtoAnalyzer 
-    attr_accessor :proto_file
+  attr_accessor :proto_file
 
-    def initialize(module_name)
-        @proto_file = ProtoFile.new(module_name)
-        @current_container = @proto_file # type could be ProtoFile, Message, EnumDef
-    end
+  def initialize(module_name)
+    @proto_file = ProtoFile.new(module_name)
+    @current_container = @proto_file # type could be ProtoFile, Message, EnumDef
+  end
 
-    def ParseProtoStructure(line)
-        case line
-        when /message\s+(\w+)\s+\{/   # message definition start
-            new_element = Message.new($1)
-            @current_container.Push new_element
-            @current_container = new_element
-        when /enum\s+(\w+)\s+\{/      # enum definition start
-            new_element = EnumDef.new($1) 
-            @current_container.Push new_element
-            @current_container = new_element
-        when /oneof\s+(\w+)\s+{/      # Oneof  definition start
-            new_element = OneofField.new($1) 
-            @current_container.Push new_element
-            @current_container = new_element   
-        when /\}/                      # '{' must be in a single line
-            @current_container = @current_container.parent
-        else
-            @current_container.ParseContents(line)
-        end
+  def ParseProtoStructure(line)
+    case line
+    when /message\s+(\w+)\s+\{/   # message definition start
+      new_element = Message.new($1)
+      @current_container.Push new_element
+      @current_container = new_element
+    when /enum\s+(\w+)\s+\{/      # enum definition start
+      new_element = EnumDef.new($1) 
+      @current_container.Push new_element
+      @current_container = new_element
+    when /oneof\s+(\w+)\s+{/      # Oneof  definition start
+      new_element = OneofField.new($1) 
+      @current_container.Push new_element
+      @current_container = new_element   
+    when /\}/                      # '{' must be in a single line
+      @current_container = @current_container.parent
+    else
+      @current_container.ParseContents(line)
     end
+  end
 end
 
 
@@ -171,14 +171,14 @@ def run_generate_external_API(input_dir, output_dir, component_name)
   erb_template_list = Dir["./ErbFile/API.*.erb"]
   erb_template_list.each do | temp_file |
     if temp_file =~ /\.\/ErbFile\/(.+)\.erb/
-        new_file_name = output_dir + @proto_obj.module_name + "_" + $1
-        f = File.new(new_file_name, "w") 
-        puts "Generating #{new_file_name}"
-        File.open(temp_file) { |fh| 
-          erb_engine = ERB.new( fh.read ) 
-          f.print erb_engine.result( binding )   
-        }
-     end
+      new_file_name = output_dir + @proto_obj.module_name + "_" + $1
+      f = File.new(new_file_name, "w") 
+      puts "Generating #{new_file_name}"
+      File.open(temp_file) { |fh| 
+        erb_engine = ERB.new( fh.read ) 
+        f.print erb_engine.result( binding )   
+      }
+    end
   end
 end
   
